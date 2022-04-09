f97eca78e6510a707cfa96952cc470f494d27be0
Samuel Budai <xbudais@gmail.com>
2022-04-10 00:37:48 +0200

    Added Ride and UserRide seeds and minor changes in Car and User Seeds.

---
+++ CarPool/CarPool.Common.Tests/Seeds/UserRideSeeds.cs
@@ -0,0 +1,51 @@
+using CarPool.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarPool.Common.Tests.Seeds;
+
+public static class UserRideSeeds
+{
+    public static readonly UserRideEntity EmptyUserRideEntity = new(
+        Id: default,
+        UserId: default,
+        RideId: default
+    )
+    {
+        User = default,
+        Ride = default
+    };
+
+    public static readonly UserRideEntity UserRideEntity1 = new(
+        Id: Guid.Parse(input: "0F8264D4-0EA1-435A-95EF-F0E5C376A6B2"),
+        UserId: UserSeeds.UserEntity2.Id,
+        RideId: RideSeeds.RideEntity1.Id
+    )
+    {
+        User = UserSeeds.UserEntity2,
+        Ride = RideSeeds.RideEntity1
+    };
+
+    public static readonly UserRideEntity UserRideEntity2 = new(
+        Id: Guid.Parse(input: "A302CC21-F393-4029-820F-2245DCBCF97A"),
+        UserId: UserSeeds.UserEntity2.Id,
+        RideId: RideSeeds.RideEntity2.Id
+    )
+    {
+        User = UserSeeds.UserEntity2,
+        Ride = RideSeeds.RideEntity2
+    };
+
+    public static readonly UserRideEntity UserRideEntityUpdate = UserRideEntity1 with { Id = Guid.Parse("98243195-CDCA-476C-9204-D7C348E2FB1E"), User = null, Ride = null};
+    public static readonly UserRideEntity UserRideEntityDelete = UserRideEntity1 with { Id = Guid.Parse("1924A46D-3F5E-4E1E-B8F5-A548EECBA03D"), User = null, Ride = null};
+
+    public static void Seed(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserRideEntity>().HasData(
+            UserRideEntity1 with { Owner = null, Rides = Array.Empty<RideEntity>() },
+            UserRideEntity2,
+            UserRideEntityUpdate,
+            UserRideEntityDelete
+        );
+    }
+
+}
