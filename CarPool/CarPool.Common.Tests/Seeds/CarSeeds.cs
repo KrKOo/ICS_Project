dd62983c04c148aa8b999b91f8f9874392b34ee8
Samuel Budai <xbudais@gmail.com>
2022-04-09 23:09:02 +0200

    Added Car and User Seeds for testing.

--- CarPool/CarPool.Common.Tests/Seeds/CarSeeds.cs
+++ CarPool/CarPool.Common.Tests/Seeds/CarSeeds.cs
@@ -1,1 +1,66 @@
-// TODO
+
+using CarPool.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarPool.Common.Tests.Seeds;
+
+public static class CarSeeds
+{
+    public static readonly CarEntity EmptyCarEntity = new(
+        Id: default, 
+        Manufacturer: default,
+        Model: default,
+        LicensePlate: default, 
+        DateOfRegistration: default,
+        PhotoUrl: default,
+        NumberOfSeats: default,
+        OwnerID: default )
+    {
+        Owner = default
+    };
+    
+    public static readonly CarEntity CarEntity1 = new(
+        Id: Guid.Parse(input: "0d4fa150-ad80-4d46-a511-4c666166ec5e"),
+        Manufacturer: "Škoda",
+        Model: "Superb",
+        LicensePlate: "1BY 1234"
+        DateOfRegistration: new DateTime(2022, 1, 1),
+        PhotoUrl: "https://www.vectorstock.com/royalty-free-vector/retro-car-vector-6547718",
+        NumberOfSeats: 4,
+        OwnerID: UserSeeds.UserEntity.Id)
+        {
+            Owner =  UserSeeds.UserEntity
+        };
+
+    public static readonly CarEntity CarEntity2 = new(
+        Id: Guid.Parse(input: "0d4fa150-ad80-4d46-a511-4c666166ec5e"),
+        Manufacturer: "Fiat",
+        Model: "Multipla",
+        LicensePlate: "2B5 5432"
+        DateOfRegistration: new DateTime(2022, 3, 2),
+        PhotoUrl: "https://www.vectorstock.com/royalty-free-vector/retro-car-vector-6547718",
+        NumberOfSeats: 4,
+        OwnerID: UserSeeds.UserEntity.Id)
+        {
+            Owner =  UserSeeds.UserEntity
+        };
+
+    public static readonly CarEntity CarEntityUpdate = CarEntity1 with { Id = Guid.Parse("A2E6849D-A158-4436-980C-7FC26B60C674"), Owner = null, Rides = Array.Empty<RideEntity>(), OwnerId = UserSeeds.UserCarsEntityUpdate.Id};
+    public static readonly CarEntity CarEntityDelete = CArEntity1 with { Id = Guid.Parse("30872EFF-CED4-4F2B-89DB-0EE83A74D279"), Owner = null, Rides = Array.Empty<RideEntity>(), OwnerId = UserSeeds.UserCarsEntityDelete.Id};
+
+    static CarSeeds()
+    {
+        CarEntity1.Rides.Add(RideSeeds.Ride1);
+        CarEntity1.Rides.Add(RideSeeds.Ride2);
+    }
+
+    public static void Seed(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<IngredientAmountEntity>().HasData(
+            CarEntity1 with { Owner = null, Rides = Array.Empty<RideEntity>() },
+            CarEntity2 with { Owner = null, Rides = Array.Empty<RideEntity>() },
+            CarEntityUpdate,
+            CarEntityDelete
+        );
+    }
+}
--- CarPool/CarPool.Common.Tests/Seeds/UserSeeds.cs
+++ CarPool/CarPool.Common.Tests/Seeds/UserSeeds.cs
@@ -6,32 +6,63 @@ namespace CarPool.Common.Tests.Seeds;
 
 public static class UserSeeds
 {
-	public static readonly UserEntity User1 = new(
-		Id: Guid.Parse(input: "06a8a2cf-ea03-4095-a3e4-aa0291fe9c75"),
-		Email: "user1@email.com",
-		FirstName: "John",
-		LastName: "Doe",
-		PhotoUrl: @"https://cdn.vectorstock.com/i/1000x1000/54/17/person-gray-photo-placeholder-man-vector-24005417.webp",
-		PhoneNumber: "+420 123 456 789",
-		DateOfBirth: new DateOnly(1980, 9, 20),
-		Info: "I like long rides."
-	);
-	public static readonly UserEntity User2 = new(
-		Id: Guid.Parse(input: "df935095-8709-4040-a2bb-b6f97cb416dc"),
-		Email: "user2@email.com",
-		FirstName: "Peter",
-		LastName: "Smith",
-		PhotoUrl: @"https://cdn.vectorstock.com/i/1000x1000/54/17/person-gray-photo-placeholder-man-vector-24005417.webp",
-		PhoneNumber: "+421 987 654 321",
-		DateOfBirth: new DateOnly(1995, 6, 30),
-		Info: "I like enjoy meeting new people."
-	);
+    public static readonly UserEntity EmptyUserEntity = new(
+        Id: default,
+        Email: default,
+        FirstName: default,
+        LastName: default,
+        PhotoUrl: default,
+        PhoneNumber: default,
+        DateOfBirth: default,
+        Info: default);
 
-	public static void Seed(this ModelBuilder modelBuilder)
-	{
-		modelBuilder.Entity<UserEntity>().HasData(
-			User1,
-			User2
-		);
-	}
+    public static readonly UserEntity UserEntity = new(
+        Id: Guid.Parse(input: "06a8a2cf-ea03-4095-a3e4-aa0291fe9c75"),
+        Email: "user1@email.com",
+        FirstName: "John",
+        LastName: "Doe",
+        PhotoUrl: @"https://cdn.vectorstock.com/i/1000x1000/54/17/person-gray-photo-placeholder-man-vector-24005417.webp",
+        PhoneNumber: "+420 123 456 789",
+        DateOfBirth: new DateOnly(1980, 9, 20),
+        Info: "I like long rides.");
+
+    //To ensure that no tests reuse these clones for non-idempotent operations
+    public static readonly UserEntity UserEntityWithNothing = UserEntity with { Id = Guid.Parse("98B7F7B6-0F51-43B3-B8C0-B5FCFFF6DC2E"), Cars = Array.Empty<CarEntity>(), RidesAsDriver = Array.Empty<RideEntity>(), RidesAsPassenger = Array.Empty<UserRideEntity>()};
+    public static readonly UserEntity UserEntityUpdate = UserEntity with { Id = Guid.Parse("0953F3CE-7B1A-48C1-9796-D2BAC7F67868"), Cars = Array.Empty<CarEntity>(), RidesAsDriver = Array.Empty<RideEntity>(), RidesAsPassenger = Array.Empty<UserRideEntity>()};
+    public static readonly UserEntity UserEntityDelete = UserEntity with { Id = Guid.Parse("5DCA4CEA-B8A8-4C86-A0B3-FFB78FBA1A09"), Cars = Array.Empty<CarEntity>(), RidesAsDriver = Array.Empty<RideEntity>(), RidesAsPassenger = Array.Empty<UserRideEntity>()};
+
+    public static readonly UserEntity UserCarsEntityUpdate = UserEntity with { Id = Guid.Parse("4FD824C0-A7D1-48BA-8E7C-4F136CF8BF31"), Cars = Array.Empty<CarEntity>(), RidesAsDriver = Array.Empty<RideEntity>(), RidesAsPassenger = Array.Empty<UserRideEntity>()};
+    public static readonly UserEntity UserCarsEntityDelete = UserEntity with { Id = Guid.Parse("F78ED923-E094-4016-9045-3F5BB7F2EB88"), Cars = Array.Empty<CarEntity>(), RidesAsDriver = Array.Empty<RideEntity>(), RidesAsPassenger = Array.Empty<UserRideEntity>()};
+
+    public static readonly UserEntity UserRidesAsDriverEntityUpdate = UserEntity with { Id = Guid.Parse("E0927FE8-99C8-4A88-9460-9B4300FD619A"), Cars = Array.Empty<CarEntity>(), RidesAsDriver = Array.Empty<RideEntity>(), RidesAsPassenger = Array.Empty<UserRideEntity>()};
+    public static readonly UserEntity UserRidesAsDriverEntityDelete = UserEntity with { Id = Guid.Parse("D66A43B8-CBE1-4497-8551-29B89B5AD371"), Cars = Array.Empty<CarEntity>(), RidesAsDriver = Array.Empty<RideEntity>(), RidesAsPassenger = Array.Empty<UserRideEntity>()};
+
+    public static readonly UserEntity UserRidesAsPassengerEntityUpdate = UserEntity with { Id = Guid.Parse("3E201B8E-7399-437F-A379-727BBCFB5E91"), Cars = Array.Empty<CarEntity>(), RidesAsDriver = Array.Empty<RideEntity>(), RidesAsPassenger = Array.Empty<UserRideEntity>()};
+    public static readonly UserEntity UserRidesAsPassengerEntityDelete = UserEntity with { Id = Guid.Parse("AD62ED05-7859-474A-9FC9-079A936E1BE5"), Cars = Array.Empty<CarEntity>(), RidesAsDriver = Array.Empty<RideEntity>(), RidesAsPassenger = Array.Empty<UserRideEntity>()};
+
+    static UserSeeds()
+    {
+        UserEntity.Cars.Add(CarSeeds.CarEntity1);
+        UserEntity.Cars.Add(CarSeeds.CarEntity2);
+
+        UserEntity.RidesAsDriver.Add(UserRideSeeds.UserRide1);
+        UserEntity.RidesAsPassenger.Add(UserRideSeeds.UserRide2);
+    } 
+
+    public static void Seed(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserEntity>().HasData(
+            UserEntity with { Cars = Array.Empty<CarEntity>(), RidesAsDriver = Array.Empty<RideEntity>(), RidesAsPassenger = Array.Empty<UserRideEntity>() },
+            UserEntityWithNothing,
+            UserEntityUpdate,
+            UserEntityDelete,
+            UserCarsEntityUpdate,
+            UserCarsEntityDelete,
+            UserRidesAsDriverEntityUpdate,
+            UserRidesAsDriverEntityDelete,
+            UserRidesAsPassengerEntityUpdate,
+            UserRidesAsPassengerEntityDelete
+
+        );
+    }
 }
