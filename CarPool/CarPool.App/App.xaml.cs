4276b149867177c2e008ab00a3a6aaf54b60d382
Martin Barborík <xbarbo10@vutbr.cz>
2022-05-06 14:13:18 +0200

    Views setup

---
+++ CarPool/CarPool.App/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using CarPool.App.Views;
+
+namespace CarPool.App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
